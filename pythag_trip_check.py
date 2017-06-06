16ce8f35fe65b1bc2704156a2afc4d5e3fc6a12f
PRM <prmwrites@gmail.com>
2017-06-06 18:08:59 -0400

    Program loops for additional questions if user desires

--- pythag_trip_check.py
+++ pythag_trip_check.py
@@ -1,27 +1,28 @@
-# Ask user for three sides of a triangle
 def ask_for_side(a):
   answer = int(input("Enter the " + a + " side of the triangle:\n"))
   return answer
 
-a = ask_for_side("first")
-b = ask_for_side("second")
-c = ask_for_side("third")
+another_question = 'yes'
 
-# sort the three inputed values from smallest to largest
-sides = [a, b, c]
-sides.sort()
+while another_question == 'yes':
+  a = ask_for_side("first")
+  b = ask_for_side("second")
+  c = ask_for_side("third")
 
-a = sides[0]
-b = sides[1]
-c = sides[2]
+  sides = [a, b, c]
+  sides.sort()
 
-# Raise each side to the 2nd power
-a = a**2
-b = b**2
-c = c**2
+  a = sides[0]
+  b = sides[1]
+  c = sides[2]
 
-# Check whether the two smallest sides raised to the second power equal the longest side raised to the second power
-if a + b == c:
-    print("\nCongratulations, your triangle is a pythagorean triple!\n")
-else:
-    print("\nSorry, your triangle isn't a pythagorean triple.\n")
+  a = a**2
+  b = b**2
+  c = c**2
+
+  if a + b == c:
+      print("\nCongratulations, your triangle is a pythagorean triple!\n")
+      another_question = input("Would you like to check another triangle? If so, type yes:")
+  else:
+      print("\nSorry, your triangle isn't a pythagorean triple.\n")
+      another_question = input("Would you like to check another triangle? If so, type yes:")
