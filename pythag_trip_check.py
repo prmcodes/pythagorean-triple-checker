ec9b1d9b2f005d7f75f2defefaab3bbcf31bd88d
PRM <prmwrites@gmail.com>
2017-06-06 16:43:22 -0400

    Initialize

---
+++ pythag_trip_check.py
@@ -0,0 +1,7 @@
+# Ask user for three sides of a triangle
+
+# sort the three inputed values from smallest to largest
+
+# Raise each side to the 2nd power
+
+# Check whether the two smallest sides raised to the second power equal the longest side raised to the second power
---
+++ test_pythag_trip_check.py
@@ -0,0 +1,9 @@
+import pythag_trip_check
+import unittest
+
+class TestAdd(unittest.TestCase):
+  def test_pythag_trip_check(self):
+      # Empty
+
+if __name__ == '__main__':
+    unittest.main()
