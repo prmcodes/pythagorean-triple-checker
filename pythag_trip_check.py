0286de93885718ccd9176eaaa3ecc88962f8c20c
PRM <prmwrites@gmail.com>
2017-06-06 17:09:31 -0400

    Added a simple function to ask for input of the three sides

--- pythag_trip_check.py
+++ pythag_trip_check.py
@@ -1,4 +1,11 @@
 # Ask user for three sides of a triangle
+def ask_for_side(a):
+  answer = int(input("Enter the " + a + " side of the triangle:"))
+  return answer
+
+a = ask_for_side("first")
+b = ask_for_side("second")
+c = ask_for_side("third")
 
 # sort the three inputed values from smallest to largest
 
